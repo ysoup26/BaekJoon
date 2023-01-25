48930ff8847afcfadb02e625d735c18125c0014a
seoyeon1359 <93582977+seoyeon1359@users.noreply.github.com>
2023-01-26 00:11:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 44 ms, Memory: 4916 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c=0;
+    string a;
+    while(cin >> a)
+    {
+        c++;
+        //cout<<c<<endl;
+    }
+    cout<<c;
+    return 0;
+}
