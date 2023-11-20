e01a215deee2fb5a4d9a9b85d9b56bcf42f10cca
seoyeon1359 <93582977+seoyeon1359@users.noreply.github.com>
2023-11-20 14:50:29 +0900

    [D1] Title: 더블더블, Time: 6 ms, Memory: 13,336 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/README.md
@@ -0,0 +1,15 @@
+# [D1] 더블더블 - 2019 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QDEX6AqwDFAUq) 
+
+### 성능 요약
+
+메모리: 13,336 KB, 시간: 6 ms, 코드길이: 1,391 Bytes
+
+### 제출 일자
+
+2023-11-20 14:50
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2019. 더블더블/더블더블.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		int N = T;
+         //2^0부터 2^N까지
+        for(int i=0;i<=N;i++)
+        {
+            int mul = 1;
+            for(int j=0;j<i;j++)
+            {
+            	mul = mul * 2;
+            }
+            cout<<mul<<" ";
+        }
+        break;
+		/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
