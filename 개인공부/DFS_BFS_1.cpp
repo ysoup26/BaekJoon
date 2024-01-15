de1070e21b6955a5aeffd431ff896d05e21c1832
ysoup26 <ysoup26@gmail.com>
2024-01-15 23:20:52 +0900

    코드 구조 수정

--- 개인공부/DFS_BFS_1.cpp
+++ 개인공부/DFS_BFS_1.cpp
@@ -7,13 +7,16 @@
 
 입력: N x M의 얼음틀 정보. N이 줄, M이 칸
 
+4 5
 0 0 1 1 0
 0 0 0 1 1
 1 1 1 1 1
 0 0 0 0 0
 
-풀이: 
-
+풀이: 0인 칸을 찾아서 dfs로 탐색을 시작하고, 
+더 이상 탐색할 것이 없으면 얼음틀 개수를 증가시킨다.
+0인 칸을 찾는 것은 2중 for문을 이용한 완전 탐색. 
+연산을 줄이기 위해서 직전 시작점을 기억해서 거기부터 탐색한다.
 */
 
 
@@ -22,11 +25,13 @@
 
 using namespace std;
 
-void list_print(int list[],int n)
-{
-    for(int i=0;i<n;i++)
-        cout<<list[i]<<" ";
-    cout<<"\n";
+int map[1000][1000];
+bool visited[1000][1000];
+int N,M;
+
+void dfs(int startX,int startY){
+    visited[startX][startY] = true;
+    
 }
 
 
@@ -34,9 +39,6 @@ int main()
 {
     int N,M,cnt;
     cin>>N>>M;
-    int ice[N][M];
-    bool visited[N][M];
-    fill(visited,visited+N*M,false);
     //입력
     for(int i=0;i<N;i++)
     {
@@ -44,10 +46,38 @@ int main()
         {
             int tmp;
             cin>>tmp;
-            ice[i][j] = tmp;
+            map[i][j] = tmp;
+            visited[i][j] = false;
         }
     }
-    dfs(ice,&cnt);
-    //list_print(list,n);
+    //얼음틀 계산
+    int startX = 0, startY = 0;
+    while(true){
+        //시작 칸 탐색
+        bool isIce = false;
+        for(int x=startX;x<N && !isIce;x++){
+            for(int y=0;y<M;y++){
+                if(map[x][y] == 0 && !visited[x][y])
+                {
+                    
+                    isIce = true;
+                    startX = x;
+                    startY = y;
+                    break;
+                }
+            }
+        }
+        
+        //시작 칸 탐색 for문이 종료되었는데 isIce가 false >> 검사할 수 있는 얼음칸이 없다.
+        if(!isIce){
+            break;
+        }
+        cout<<"x:y >>"<<startX<<":"<<startY<<"\n";
+        dfs(startX,startY);
+        cnt++;
+        
+    }
+    
+    cout<<cnt;
     return 0;
 }
