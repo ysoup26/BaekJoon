99380128c40e37004a141f79a2ac93ea4665fb3d
ysoup26 <ysoup26@gmail.com>
2024-02-09 00:01:13 +0900

    update

--- 개인공부/최단경로_1.cpp
+++ 개인공부/최단경로_1.cpp
@@ -1,24 +1,27 @@
-/*최단경로 중 다익스트라 알고리즘
-  : 현재 기준 최소 노드를 선택하고, 
-    연결되는 노드의 최소값을 함께 갱신시킴
+/*최단경로 중 플로이드 워셜
+  : 출발 노드와 도착 노드 사이에 중점 노드를 이용한다.
+    중점 노드를 매번 다르게 하여 값을 구하고 최소일때 갱신한다.
 
 입력: 
-6 11
-1 
-1 2 2
-1 3 5
-1 4 1
-2 3 3
-2 4 2
-3 2 3
-3 6 5
-4 3 3
-4 5 1
-5 3 1
-5 6 2
+n
+m
+m개의 a,b,c (출발 노드, 도착노드, 비용)
+
+4
+7
+1 2 4
+1 4 6
+2 1 3
+2 3 7
+3 1 5
+3 4 4
+4 3 2
 
 정답: 
-0 2 3 1 2 4
+0 4 8 6
+3 0 7 9
+5 9 0 4
+7 11 2 0
 
 
 */
@@ -29,73 +32,49 @@
 #define INF 987654321
 using namespace std;
 
-bool visited[100+1];
-int dist[100+1];
-vector<pair<int,int>> graph[100+1];
-int N,M;
-
-void print_node_dist(){
-    for(int i=1;i<=N;i++){
-        cout<<dist[i]<<" ";
-    }
-}
-
-int get_min_node(){
-    int min_node;
-    int min_dist = INF;
-    for(int i=1;i<=N;i++){
-        if(min_dist > dist[i] && !visited[i])
-        {
-            min_dist = dist[i];
-            min_node = i;
-        }
-    }
-    return min_node;
-}
-
-void dijkstra(int start){
-    //시작노드를 방문처리하고, 연결된 정점의 거리를 업데이트
-    visited[start] = true;
-    dist[start] = 0;
-    for(int j=0;j<graph[start].size();j++){
-        pair<int,int> p =graph[start][j];
-        dist[p.first] = p.second;
-    }
-    //시작노드를 제외한 노드들에 방문함
-    for(int i=2;i<=N;i++){
-        //연결된 정점 중, 비용이 가장 적게드는 정점을 선택한다.
-        int now_node = get_min_node();
-        visited[now_node] = true;
-        //갱신될 수 있는 정점의 거리를 갱신한다
-        for(int j=0;j<graph[now_node].size();j++){
-            pair<int,int> p =graph[now_node][j];
-            int next_node = p.first;
-            int next_dist = p.second;
-            int cost = dist[now_node] + next_dist;
-            if(dist[next_node] > cost){
-                dist[next_node] = cost;
-            }
-        }
-    }
-    
-    
-
-}
 
 int main(){
     //노드와 간선 개수, 시작 노드 등을 입력받고 초기화함
-    int start;
-    cin>>N>>M>>start;
-    fill(visited,visited+101,false);
-    fill(dist,dist+101,INF);
+    int N,M;
+    cin>>N>>M;
+    int graph[N+1][N+1];
+    //초기상태를 모두 INF로 초기화
+    for(int a=1;a<=N;a++){
+        for(int b=1;b<=N;b++){
+                graph[a][b]=INF;
+        }
+    }
+    //자기 자신에게 가는 비용은 0으로
+    for(int a=1;a<=N;a++){
+        for(int b=1;b<=N;b++){
+            if(a==b)
+                graph[a][b]=0;
+        }
+    }
+    //각 간선에 대한 정보를 입력받음
     for(int i=1;i<=M;i++){
         int a,b,c;
         cin>>a>>b>>c;
-        graph[a].push_back(make_pair(b,c));
+        graph[a][b] = c;
     }
-    //다익스트라 알고리즘 실행
-    dijkstra(start);
-    //각 거리 출력
-    print_node_dist();
+    //플로이드 워셜 알고리즘 
+    for(int k=1;k<=N;k++){
+        for(int a=1;a<=N;a++){
+            for(int b=1;b<=N;b++){
+                graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b]);
+            }
+        }
+    }
+    //결과 출력
+    for(int a=1;a<=N;a++){
+        for(int b=1;b<=N;b++){
+            if(graph[a][b] == INF)
+                cout<<"INF";
+            else
+                cout<<graph[a][b];
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
     return 0;
 }
