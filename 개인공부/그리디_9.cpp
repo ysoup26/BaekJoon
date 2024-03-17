caa6f6554d638f14c7b880892b434f751f36c9e5
ysoup26 <ysoup26@gmail.com>
2024-03-17 10:27:28 +0900

    Create 그리디_9.cpp

---
+++ 개인공부/그리디_9.cpp
@@ -0,0 +1,23 @@
+/*무지의 먹방 라이브 ** 
+
+문제 요약: 
+입력: 
+
+5 3
+1 3 2 3 2
+
+8
+
+풀이: 
+     
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> food_times, long long k) {
+    int answer = 0;
+    return answer;
+}
