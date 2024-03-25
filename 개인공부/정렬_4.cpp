3947e706fa503ee9561c9f7980d4d12759748fe7
ysoup26 <ysoup26@gmail.com>
2024-03-25 23:49:56 +0900

    Create 정렬_4.cpp

---
+++ 개인공부/정렬_4.cpp
@@ -0,0 +1,29 @@
+/*국영수
+
+1초 256MB
+
+문제 요약:
+
+입력: 
+
+풀이: 
+*/
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void list_print(int list[],int n)
+{
+    for(int i=0;i<n;i++)
+        cout<<list[i]<<" ";
+    cout<<"\n";
+}
+
+int main()
+{
+    
+    return 0;
+}
