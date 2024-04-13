434171bdf3028cfeb1aa43f060f86c6522fd2194
ysoup26 <ysoup26@gmail.com>
2024-04-13 21:47:50 +0900

    Update 구현_8.cpp

--- 개인공부/구현_8.cpp
+++ 개인공부/구현_8.cpp
@@ -1,11 +1,23 @@
-/*
+/* 자물쇠와 열쇠
 
 1초 128MB
 
-문제 요약: 
+문제 요약: 자물쇠와 열쇠가 있다.
+           자물쇠는 NxN의 형태, 열쇠는 MxM의 형태일때, 
+           열쇠를 회전하고 이동해서 자물쇠 열어야 하는데 결과에 따라 true false를 출력한다.
+           단, 열쇠의 돌기와 자물쇠의 돌기가 만나면 안되고,
+            자물쇠의 홈은 다 채워져야 한다.
 
 입력: 
+key와 lock의 2차원 배열들
+0 0 0
+1 0 0
+0 1 1
+1 1 1
+1 1 0
+1 0 1
 
+true
 
 풀이: 
 
@@ -17,52 +29,8 @@
 
 using namespace std;
 
-//두 문자열을 비교하는 함수, 인덱스 슬라이싱과 비교를 한번에
-bool same_check(string s, int idx,int n){
-    string s1="",s2="";
-    if(idx+n>=s.length()) //s2가 s를 오버하는 범위를 갖는다면 잘못된 접근
-    {    
-        return false;
-    }
-    for(int i=0;i<n;i++){
-        s1+=s[idx+i];
-        s2+=s[idx+i+n];
-    }
-    if(s1 == s2)
-        return true;
-    else
-        return false;
-}
-
-int solution(string s) {
-    int answer = 0;
-    for(int n=s.length()/2;n>0;n--){
-        //cout<<"단위: "<<n;
-        int idx = 0;
-        bool IsSame = false;
-        int n_answer = 0;
-        while(true){
-            if(idx>=s.length()) //idx가 문자열을 넘어서면 종료, 
-            {                   //idx를 n만큼 증가시키기 때문에 넘어간 부분만큼 빼줌
-                n_answer-=(idx-s.length());     
-                break;
-            }
-            if(same_check(s,idx,n)){
-                IsSame = true;
-            }else if(IsSame && !same_check(s,idx,n)){
-                n_answer+=(n+1);
-                IsSame = false;
-            }else{
-                n_answer+=n;
-            }
-            idx+=n;
-        }
-        //cout<<" 결과비교:"<<answer<<" "<<n_answer<<"\n";
-        if(answer == 0)
-            answer = n_answer;
-        if(n_answer<answer)
-            answer = n_answer;
-    }
+bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
+    bool answer = true;
     return answer;
 }
 
