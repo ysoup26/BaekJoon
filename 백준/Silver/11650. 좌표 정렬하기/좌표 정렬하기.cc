edfa482f7155e17c15a5257f126d75ad1a166083
ysoup26 <93582977+ysoup26@users.noreply.github.com>
2024-04-14 22:53:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 128 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct XY{
+    int x,y;
+};
+
+int cmd_XY(XY xy1,XY xy2){
+    if(xy1.x == xy2.x) //x는 같을 때 -> y비교
+        return xy1.y < xy2.y;
+    return xy1.x < xy2.x;
+}
+
+int main(){
+    int N;
+    cin>>N;
+    vector<XY> arr;
+    for(int i=0;i<N;i++){
+        XY inputXY;
+        cin>>inputXY.x>>inputXY.y;
+        arr.push_back(inputXY);
+    }
+    sort(arr.begin(),arr.end(),cmd_XY);
+    for(int i=0;i<N;i++){
+        cout<<arr[i].x<<" "<<arr[i].y<<"\n";
+    }
+    return 0;
+}
