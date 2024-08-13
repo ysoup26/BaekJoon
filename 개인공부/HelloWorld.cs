0669d16379d43dbe3bea819093c8304d6477e6a0
ysoup26 <ysoup26@gmail.com>
2024-08-13 22:40:25 +0900

    C#공부

---
+++ 개인공부/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.ReqularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Hello, wolrd!");
+        }
+    }
+}
