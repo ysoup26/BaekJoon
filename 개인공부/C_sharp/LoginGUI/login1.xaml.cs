82a46d819abd2a1b9067c5a2c1407e8572c8cdc9
ysoup26 <ysoup26@gmail.com>
2024-09-11 15:35:00 +0900

    화면 크기 조절로 인한 투명화 이미지 삭제

--- 개인공부/C_sharp/LoginGUI/login1.xaml.cs
+++ 개인공부/C_sharp/LoginGUI/login1.xaml.cs
@@ -1,19 +1,19 @@
-<Window x:Class="WpfGUI.View.Login1"
+<Page x:Class="WpfGUI.View.Login1"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
         xmlns:local="clr-namespace:WpfGUI.View"
         mc:Ignorable="d"
-        Title="Window1" Width="1280" Height="720">
-    <Window.Resources>
+        Title="Window1" Height="1080"  Width="1920" >
+    <Page.Resources>
         <ControlTemplate x:Key="roundbutton" TargetType="Button">
             <Grid>
                 <Ellipse Fill="{TemplateBinding Background}" Stroke="{TemplateBinding Foreground}"/>
                 <ContentPresenter HorizontalAlignment="Center" VerticalAlignment="Center"/>
             </Grid>
         </ControlTemplate>
-    </Window.Resources>
+    </Page.Resources>
     <Grid>
         <Image Source="/View/1-1.jpg" Stretch="Fill">
             <Image.Effect>
@@ -32,13 +32,11 @@
                 <ColumnDefinition Width="1.7*"></ColumnDefinition>
                 <ColumnDefinition Width="2*"></ColumnDefinition>
             </Grid.ColumnDefinitions>
-            <Border Grid.Row="1" Grid.Column="1" BorderBrush="White" BorderThickness="0.8" CornerRadius="20">
+            <Border Grid.Row="1" Grid.Column="1" BorderBrush="Transparent" BorderThickness="2" CornerRadius="20" Background="#FF2E4567">
                 <Grid>
-                    <Grid.Background>
-                        <ImageBrush ImageSource="/View/1-blur.jpg" Stretch="None"/>
-                    </Grid.Background>
+                    
                     <Grid.Clip>
-                        <RectangleGeometry RadiusX="20" RadiusY="20" Rect="0,0,375.78,486"/>
+                        <RectangleGeometry RadiusX="20" RadiusY="20" Rect="0,0,568.63,767.43"/>
                     </Grid.Clip>
                     <Grid.RowDefinitions>
                         <RowDefinition Height="1.5*"></RowDefinition>
@@ -48,18 +46,18 @@
                     <Grid Grid.Row="0">
                         <StackPanel Orientation="Vertical" VerticalAlignment="Bottom">
                             <StackPanel Orientation="Horizontal" HorizontalAlignment="Center">
-                                <Border Padding="20 4" Margin="0 0 10 0" Background="#FF4C5F7C" CornerRadius="15" BorderBrush="Transparent" BorderThickness="1">
+                                <Border Padding="30 6" Margin="0 0 10 0" Background="#FF4C5F7C" CornerRadius="15" BorderBrush="Transparent" BorderThickness="1">
                                     <Grid>
                                         <Button  Background="Transparent" BorderBrush="Transparent" Foreground="White" >Sign In</Button>
                                     </Grid>
                                 </Border>
-                                <Border Padding="20 4" Background="#FF14233F" CornerRadius="15" BorderBrush="Transparent" BorderThickness="1">
+                                <Border Padding="30 5" Background="#FF14233F" CornerRadius="15" BorderBrush="Transparent" BorderThickness="1">
                                     <Grid>
                                         <Button  Background="Transparent" BorderBrush="Transparent" Foreground="White" >Sign Up</Button>
                                     </Grid>
                                 </Border>
                             </StackPanel>
-                            <TextBlock Margin="20" Text="Sign Up" Foreground="White" FontSize="24" HorizontalAlignment="Center" VerticalAlignment="Bottom" FontWeight="Bold"/>
+                            <TextBlock Margin="20" Text="Sign Up" Foreground="White" FontSize="30" HorizontalAlignment="Center" VerticalAlignment="Bottom" FontWeight="Bold"/>
                         </StackPanel>
                     </Grid>
                     <Grid Grid.Row="1">
@@ -70,7 +68,7 @@
                             <RowDefinition Height="0.5*"/>
                             <RowDefinition Height="1*"/>
                         </Grid.RowDefinitions>
-                        <Border Grid.Row="0" Margin="20 8" CornerRadius="25" 
+                        <Border Grid.Row="0" Margin="30 15" CornerRadius="25" 
                                 BorderBrush="Transparent" BorderThickness="1" Background="#FF4C5F7C">
                             <Grid>
                                 <TextBox 
@@ -84,7 +82,7 @@
                                         HorizontalAlignment="Right"/>
                             </Grid>
                         </Border>
-                        <Border Grid.Row="1" Margin="20 8" CornerRadius="25" 
+                        <Border Grid.Row="1" Margin="30 15" CornerRadius="25" 
                                 BorderBrush="Transparent" BorderThickness="1" Background="#FF4C5F7C">
                             <Grid>
                                 <TextBox 
@@ -98,7 +96,7 @@
                                         HorizontalAlignment="Right"/>
                             </Grid>
                         </Border>
-                        <Border Grid.Row="2" Margin="20 8" CornerRadius="25" 
+                        <Border Grid.Row="2" Margin="30 15" CornerRadius="25" 
                                 BorderBrush="Transparent" BorderThickness="1" Background="#FF4C5F7C">
                             <Grid>
                                 <TextBox 
@@ -112,12 +110,12 @@
                                     HorizontalAlignment="Right"/>
                             </Grid>
                         </Border>
-                        <Border  Grid.Row="3" Margin="20 0 " CornerRadius="25" BorderBrush="Transparent" BorderThickness="1">
+                        <Border  Grid.Row="3" Margin="30 0 " CornerRadius="25" BorderBrush="Transparent" BorderThickness="1">
                             <CheckBox Foreground="White" HorizontalAlignment="Right" VerticalAlignment="Center">Remember me</CheckBox>
                         </Border>
-                        <Border Grid.Row="4" Margin="20 8" Background="#FF14233F" CornerRadius="25" BorderBrush="Transparent" BorderThickness="1">
+                        <Border Grid.Row="4" Margin="30 15" Background="#FF14233F" CornerRadius="25" BorderBrush="Transparent" BorderThickness="1">
                             <Grid>
-                                <Button  Background="Transparent" BorderBrush="Transparent" Foreground="White" >Sign up -></Button>
+                                <Button  Background="Transparent" BorderBrush="Transparent" Foreground="White" Click="Button_Click" >Sign up -></Button>
                             </Grid>
                         </Border>
                     </Grid>
@@ -147,5 +145,5 @@
             </Border>
         </Grid>
     </Grid>
-</Window>
+</Page>
    
